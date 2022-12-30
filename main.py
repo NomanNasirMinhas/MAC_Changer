cc85b2d1e7b93c70f127a5dfac9a96237b9c242c
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2022-12-31 00:12:15 +0500

    Extra Args removed

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def mac_changer(iface, mac):
         if mac_pattern.match(mac):
             subprocess.check_output(["ifconfig", iface], stdin=None, stderr=None, shell=False, universal_newlines=False)
             print("[+] Changing MAC Address of interface of " + iface + " to " + mac)
-            subprocess.call(["sudo", "ifconfig", iface, "down"])
+            # subprocess.call(["sudo", "ifconfig", iface, "down"])
             subprocess.call(["sudo", "ifconfig ", iface, "hw", "ether", mac])
-            subprocess.call(["sudo", "ifconfig ", iface, "up"])
+            # subprocess.call(["sudo", "ifconfig ", iface, "up"])
         else:
             print("[-] Invalid MAC Address")
             return False
