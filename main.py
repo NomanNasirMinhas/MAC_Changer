18271d3289c17954c7b28fa4c63f40da00ee5655
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2022-12-31 00:09:39 +0500

    Code Revert

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import subprocess
 import optparse
 import re
-import time
 
 mac_pattern = re.compile(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w")
-
 
 def get_args():
     parser = optparse.OptionParser()
@@ -20,61 +18,35 @@ def get_args():
     else:
         return options
 
-def check_outcome(iface):
-    result = subprocess.check_output(["ifconfig", iface], stdin=None, stderr=None, shell=False,
-                                     universal_newlines=False)
-    mac_search_res = re.search(mac_pattern, result.decode("utf-8"))
-    if mac_search_res:
-        if iface == mac_search_res.group(0):
-            print("[+] Interface MAC Address has been spoofed successfully")
-            return True
-        else:
-            print("[+] Confirming if MAC has been spoofed successfully")
-            return False
-
-    else:
-        print("[-] MAC address not found for provided interface.")
-        return 0
 
 def mac_changer(iface, mac):
     try:
         if mac_pattern.match(mac):
             subprocess.check_output(["ifconfig", iface], stdin=None, stderr=None, shell=False, universal_newlines=False)
-            # subprocess.call(["sudo", "ifconfig", iface, "down"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            # condition = True # Condition to See if Interface is down
-            # while condition:
-            #     result = subprocess.check_output(["ifconfig", iface], stdin=None, stderr=None, shell=False,
-            #                                      universal_newlines=False).decode("utf-8")
-            #     if "inet" in result:
-            #         print("[+] Waiting for " + iface + " status to change to down..")
-            #         time.sleep(2)
-            #     else:
-            #         condition = False
-            # print("[+] Changing MAC Address of interface of " + iface + " to " + mac)
-            # subprocess.call(["sudo", "ifconfig ", iface, "hw", "ether", mac], stdout=subprocess.DEVNULL,
-            #                 stderr=subprocess.STDOUT)
-            condition = True # Condition to See if MAC Address Has been spoofed
-            count = 1
-            while condition:
-                if count < 10:
-                    if check_outcome(iface):
-                        print("[+] MAC Address changed successfully")
-                    elif not check_outcome(iface):
-                        print("[+] Waiting for " + iface + " MAC Change..")
-                        count = count + 1
-                        time.sleep(2)
-                    elif check_outcome(iface) == 0:
-                        return
-
-                else:
-                    print("[-] MAC Address Could not be spoofed")
-            # subprocess.call(["sudo", "ifconfig ", iface, "up"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            print("[+] Changing MAC Address of interface of " + iface + " to " + mac)
+            subprocess.call(["sudo", "ifconfig", iface, "down"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            subprocess.call(["sudo", "ifconfig ", iface, "hw", "ether", mac], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            subprocess.call(["sudo", "ifconfig ", iface, "up"], shell=True,  stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         else:
             print("[-] Invalid MAC Address")
             return False
     except:
         print("[-] Something Went Wrong")
         return False
+
+
+
+def check_outcome(iface):
+    result = subprocess.check_output(["ifconfig", iface], stdin=None, stderr=None, shell=False, universal_newlines=False)
+    mac_search_res = re.search(mac_pattern, result.decode("utf-8"))
+    if mac_search_res:
+        if iface == mac_search_res.group(0):
+            print("[+] Interface MAC Address has been spoofed successfully")
+        else:
+            print("[-] Interface MAC Address could not be spoofed")
+
+    else:
+        print("[-] MAC address not found for provided interface.")
 
 
 args = get_args()
