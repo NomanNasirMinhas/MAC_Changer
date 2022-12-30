1988f5775f8edd7760f0b256c21d087b382f3d38
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2022-12-30 21:40:35 +0500

    mac_changer function added

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address of the se
 (options, args) = parser.parse_args()
 interface = options.interface
 new_mac = options.new_mac
-print("[+] Changing MAC Address of interface of " + interface + " to -> " + new_mac)
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
 
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig ",interface,"hw","ether",new_mac])
-subprocess.call(["ifconfig ",interface,"up"])
+
+def mac_changer(iface, mac):
+    print("[+] Changing MAC Address of interface of " + iface + " to -> " + new_mac)
+    # subprocess.call("ifconfig " + interface + " down", shell=True)
+    # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+    # subprocess.call("ifconfig " + interface + " up", shell=True)
+
+    subprocess.call(["ifconfig", iface, "down"])
+    subprocess.call(["ifconfig ", iface, "hw", "ether", mac])
+    subprocess.call(["ifconfig ", iface, "up"])
+
+
+mac_changer(interface, new_mac)
