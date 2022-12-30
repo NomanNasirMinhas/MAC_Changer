c8de26b66ae27728498f7bc0329d0d7427c282c4
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2022-12-30 21:22:59 +0500

    CMD Line Args Added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import subprocess
+import optparse
 
-interface = input("Enter Interface Name: ")
-new_mac = input("Enter New MAC Address: ")
+parser = optparse.OptionParser()
+parser.add_option("-i", "--i", dest="interface", help="Interface of which MAC address is to be changed")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address of the selected interface")
+(options, args) = parser.parse_args()
+interface = options.interface
+new_mac = options.new_mac
 print("[+] Changing MAC Address of interface of " + interface + " to -> " + new_mac)
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig ",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig ",interface,"up"])
